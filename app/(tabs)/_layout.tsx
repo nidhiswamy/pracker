067bf531b26feaf7d8675434836e3698fc1fd2cd
nidhiswamy <nms6241@psu.edu>
2024-09-01 13:34:45 -0600

    intial commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,6 +32,12 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+        }}
+      />
     </Tabs>
   );
 }
